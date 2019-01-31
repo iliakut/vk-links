3f4bd9bdaabe3f9c99bada6b1bc2946ce7922668
Ilia <ilyakut@gmail.com>
2019-01-31 15:53:28 +0300

    added sets and Object of result

--- server/src/index.js
+++ server/src/index.js
@@ -15,40 +15,44 @@ app.use(cors());
 app.listen(process.env.PORT || config.port,
   () => console.log(`Server start on port ${config.port} ...`));
 
-let test = '10';
-
-
-app.get('/', (req, res) => {
-  res.send(test);
+/*app.get('/', (req, res) => {
+  res.send('server ok');
   console.log(req);
-});
+});*/
 
 app.post('/', function(req, res) {
   // check reseived information
-  let id = req.body.id;
-  let friends_list = {};
-  console.log('getting data from VK-API');
-  // get API options
-  const options = {
+  let id1 = req.body.id;
+  let id_list = {
+    'set': 0,
+    'len': 0,
+    'id': 0
+  };
+  let options = {
     method: 'GET',
     uri: 'https://api.vk.com/method/friends.get?v=5.52&',
     qs: {
       access_token: 'b07354b5b07354b5b07354b5d8b01b4b52bb073b07354b5ec32f7b936771553438e7f17',
-      user_id: id,
+      user_id: id1,
       order: 'hints'
     },
     json: true
   };
-  let data = []; // вспомогательный массив для записи в файл
+
+  console.log('getting data from VK-API');
   // get data from vk-API
   request_promise(options)
     .then(function (response) {
       let resp = response;
-      // запишем в файл
-      data.push(JSON.stringify(resp) + '\n\n\n');
-      console.log(data.length);
-      fs.writeFileSync("data.txt", data);
 
+      id_list.set = new Set(resp.response.items);
+      id_list.len = resp.response.count;
+
+      id_list.set.forEach(
+////?????????????????????????
+      );
+
+      fs.writeFileSync("data.txt", JSON.stringify(Array.from(id_list.set)));
       //res.send(resp); отправить на клиента
     })
     .catch(function (err) {
