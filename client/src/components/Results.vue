251c21558386758d6095e333965a681716938b55
Ilia <ilyakut@gmail.com>
2019-02-28 16:12:45 +0300

    line

--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -1,5 +1,17 @@
 <template>
-
+  <v-container>
+    <v-layout align-center column>
+      <v-flex xs8>
+        <v-avatar color="grey lighten-4">
+          <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+        </v-avatar>
+        <svg width="100%" height="10%"><line x1="0px" y1="50px" x2="15px" y2="50px" stroke="red" stroke-width="9"></line></svg>
+        <v-avatar color="grey lighten-4">
+          <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+        </v-avatar>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -7,3 +19,10 @@
     name: "Results"
   }
 </script>
+
+<style scoped>
+  svg {
+    width: 48px;
+    height: 20px;
+  }
+</style>
