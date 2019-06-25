68ef111b02f56d2d383b93b494d8a59553f511f4
Ilia <ilyakut@gmail.com>
2019-06-25 16:10:18 +0300

    validation of the link

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -6,7 +6,7 @@
           <v-text-field
             v-model="first_id"
             label="первый пользователь"
-            placeholder="id1"
+            placeholder="https:/vk.com/first_user"
             :rules="rules"
             required
             outline
@@ -23,7 +23,7 @@
           <v-text-field
             v-model="second_id"
             label="второй пользователь"
-            placeholder="id2"
+            placeholder="https:/vk.com/second_user"
             :rules="rules"
             required
             outline
@@ -43,13 +43,13 @@ import { mapState, mapMutations } from "vuex";
 export default {
   name: "Search",
   data: () => ({
-    first_id: "17784637",
-    second_id: "15278385",
+    first_id: "",
+    second_id: "",
     valid: false,
     rules: [
       v => (v || "").indexOf(" ") < 0 || "Пробелы недопустимы",
-      v => !!v || "Обязательное поле"
-      //v => /https:\/\/vk.com\//.test(v) || 'Не похоже на ссылку' // - закомментировал на время разработки
+      v => !!v || "Обязательное поле",
+      v => /https:\/\/vk.com\//.test(v) || 'Не похоже на ссылку' // - закомментировал на время разработки
     ]
   }),
 
@@ -57,7 +57,7 @@ export default {
     sendName: function() {
       this.changeStatus(1);
       axios
-        .post("http://localhost:8081/", { id1: this.first_id, id2: this.second_id })
+        .post("http://localhost:8081/", { id1: this.first_user_screenName, id2: this.second_user_screenName })
         .then(response => {
           console.log(response);
           this.writeMutualData(response.data);
@@ -76,14 +76,22 @@ export default {
         // if valid
         this.sendName(); // send data to server
         this.$router.push("/result");
-        this.setFirstId(this.first_id);
-        this.setSecondId(this.second_id);
+        this.setFirstId(this.first_user_screenName);
+        this.setSecondId(this.second_user_screenName);
       }
     },
     ...mapMutations(["changeStatus", "writeMutualData", "setFirstId", "setSecondId"])
   },
   computed: {
-    ...mapState(["searchStatus"])
+    ...mapState(["searchStatus"]),
+    first_user_screenName: function () {
+      let index = this.first_id.indexOf("com/");
+      return this.first_id.slice(index + 4, this.first_id.length);
+    },
+    second_user_screenName: function () {
+      let index = this.second_id.indexOf("com/");
+      return this.second_id.slice(index + 4, this.second_id.length);
+    }
   }
 };
 </script>
